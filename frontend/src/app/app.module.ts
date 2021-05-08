3dcd44f4bb8b0825638a7a4f21d55f04e1af4f0a
Rafael Dantas <rafaeldantasds@gmail.com>
2021-05-08 15:21:42 -0300

    Created login screen

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,11 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ChatComponent } from './chat/chat.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+      ChatComponent,
+      LoginComponent
+   ],
   imports: [
     BrowserModule
   ],
