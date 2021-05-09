cd6d1523b589d713048cb55ed5368fd03d8c0d53
Rafael Dantas <rafaeldantasds@gmail.com>
2021-05-09 20:29:38 -0300

    Ajuste no chat / centralização dos compontentes na tela

--- src/app/chat/players-list/players-list.component.ts
+++ src/app/chat/players-list/players-list.component.ts
@@ -13,50 +13,32 @@ export class PlayersListComponent implements OnInit {
 
   players = [
     {
-      id: 1,
-      name: "Player 1",
-      checked: false
+      id: 1, name: "Player 1", checked: false
     },
     {
-      id: 2,
-      name: "Player 2",
-      checked: false
+      id: 2, name: "Player 2", checked: false
     },
     {
-      id: 3,
-      name: "Player 3",
-      checked: false
+      id: 3, name: "Player 3", checked: false
     },
     {
-      id: 4,
-      name: "Player 4",
-      checked: false
+      id: 4, name: "Player 4", checked: false
     },
     {
-      id: 5,
-      name: "Player 5",
-      checked: false
+      id: 5, name: "Player 5", checked: false
     },
     {
-      id: 6,
-      name: "Player 6",
-      checked: false
+      id: 6,name: "Player 6", checked: false
     },
     {
-      id: 7,
-      name: "Player 7",
-      checked: false
+      id: 7, name: "Player 7", checked: false
     },
     {
-      id: 8,
-      name: "Player 8",
-      checked: false
+      id: 8, name: "Player 8", checked: false
     },
     {
-      id: 9,
-      name: "Player 9",
-      checked: false
-    },
+      id: 9, name: "Player 9", checked: false
+    }
   ];
   ngOnInit() {}
 
